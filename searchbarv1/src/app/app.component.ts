f32b8653797c3789a1b82e804fe5cc133701ce14
Momčilo Miković <momcilomikovic@Momcilos-MacBook-Pro.local>
2022-12-12 16:21:51 +0100

    added a search bar with an opening animation & clear option

--- searchbarv1/src/app/app.component.ts
+++ searchbarv1/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'searchbarv1';
+  search : any;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.search = document.getElementById('mysearch');
+  }
+
+  
+  public openSearch() {
+    document.getElementById('search')?.classList.toggle('active');
+  }
+
+  public clear() {
+    this.search.value = '';
+  }
+
 }
